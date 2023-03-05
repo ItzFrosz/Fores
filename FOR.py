a977db637a9ce7a65f0d3c2eaac1a8d29adab182
ItzFrosz <shaposhnyk.artem@gmail.com>
2023-03-05 18:49:29 +0200

    3 tasks of for

---
+++ FOR.py
@@ -0,0 +1,31 @@
+#1
+if 1 == 2:
+    N = 10
+    K = 20
+    print(range (N))
+    for x in range(N):
+        print(K)
+#2
+if 2939>238489:
+    A = 20
+    B = 45
+    i = 0
+    for l in range(A,B+1):
+        print(l)
+        i = i+1
+    print(i)
+#3
+A = 203
+B = 240
+B = B-1
+N = 0
+for l in range(B,A,-1):
+    print(l)
+    N = N+1
+print(N)
+    
+    
+
+
+
+            
